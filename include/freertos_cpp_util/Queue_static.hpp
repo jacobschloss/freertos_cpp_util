6baee6a92a3b90ac3cf194d08ee28c94a91790c3
Jacob Schloss <jacob@schloss.io>
2018-12-22 15:24:25 -0800

    mem leak in Queue_static

--- include/freertos_cpp_util/Queue_static.hpp
+++ include/freertos_cpp_util/Queue_static.hpp
@@ -23,24 +23,56 @@ public:
 	{
 		
 	}
-//	~Queue_static() override
-//	{
-//
-//	}
+	~Queue_static() override
+	{
+		T* ptr = nullptr;
+		while(m_alloc_queue.pop_front(ptr))
+		{
+			m_pool.free(ptr);
+		}
+	}
 
-	bool push_back(const T& item)
+	bool push_back(const T& item) override
 	{
 		return push_back(item, 0);
 	}
 
-	bool push_back(const T& item, const TickType_t xTicksToWait)
+	bool push_back(const T& item, const TickType_t xTicksToWait) override
 	{
 		T* ptr = m_pool.try_allocate(xTicksToWait, item);
+
+		if(!ptr)
+		{
+			return false;
+		}
 
 		if(!m_alloc_queue.push_back(ptr, 0))
 		{
 			return false;
 		}
+
+		return true;
+	}
+
+	bool pop_front(T* const item) override
+	{
+		return pop_front(item, 0);
+	}
+
+	bool pop_front(T* const item, const TickType_t xTicksToWait) override
+	{
+		//do we have one?
+		T* ptr = nullptr;
+		if(!m_alloc_queue.pop_front(ptr, xTicksToWait))
+		{
+			return false;
+		}
+
+		//copy over
+		*item = ptr;
+
+		//free our internal copy
+		m_pool.free(ptr);
 
 		return true;
 	}
