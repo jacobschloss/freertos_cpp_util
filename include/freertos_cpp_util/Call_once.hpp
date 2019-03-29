393723d2d1f2f5f79ce3cf42359b031d35600e44
Jacob Schloss <jacob@schloss.io>
2019-03-29 13:37:36 -0700

    lic info

--- include/freertos_cpp_util/Call_once.hpp
+++ include/freertos_cpp_util/Call_once.hpp
@@ -1,3 +1,9 @@
+/**
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2018 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #pragma once
 
 #include "freertos_cpp_util/Mutex_static.hpp"
