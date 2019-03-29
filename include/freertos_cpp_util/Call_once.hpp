be759283e6d8778c2a8ebe4ffcd9ce7cafb9ef35
Jacob Schloss <jacob@schloss.io>
2019-03-29 13:35:36 -0700

    a call once flag for freertos

---
+++ include/freertos_cpp_util/Call_once.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "freertos_cpp_util/Mutex_static.hpp"
+
+#include <atomic>
+#include <mutex>
+#include <version>
+
+class Once_flag;
+
+template<class Callable, class... Args>
+void call_once(Once_flag& flag, Callable&& f, Args&&... args);
+
+class Once_flag
+{
+	protected:
+
+	template<class Callable, class... Args>
+	friend void call_once(Once_flag& flag, Callable&& f, Args&&... args);
+
+	std::atomic<bool> m_flag;
+
+	Mutex_static m_mutex;
+};
+
+template<class Callable, class... Args>
+void call_once(Once_flag& flag, Callable&& f, Args&&... args)
+{
+	if(!flag.m_flag.load())
+	{
+		std::lock_guard<Mutex_static> lock(flag.m_mutex);
+		
+		if(!flag.m_flag.load())
+		{
+			#if defined(__cpp_lib_invoke) && (__cpp_lib_invoke >= 201411L)
+				std::invoke(f, std::forward<Args>(args)...);
+			#else
+				auto func = std::bind(f, std::forward<Args>(args)...);
+				func();
+			#endif
+
+			flag.m_flag.store(true);	
+		}
+	}
+}
