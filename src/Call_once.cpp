1fe59039691be3fd7bd668859d741289359041b3
Jacob Schloss <jacob@schloss.io>
2019-03-29 21:25:59 -0700

    fix date

--- src/Call_once.cpp
+++ src/Call_once.cpp
@@ -1,6 +1,6 @@
 /**
  * @author Jacob Schloss <jacob@schloss.io>
- * @copyright Copyright (c) 2018 Jacob Schloss. All rights reserved.
+ * @copyright Copyright (c) 2019 Jacob Schloss. All rights reserved.
  * @license Licensed under the 3-Clause BSD license. See LICENSE for details
 */
 
