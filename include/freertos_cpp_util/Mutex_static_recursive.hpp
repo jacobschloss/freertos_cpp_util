c9e99fdc0159b8ef53d1bf8d9d4cd96e4aa2135e
Jacob Schloss <jacob@schloss.io>
2019-04-15 13:22:28 -0700

    Mutex_static_recursive

---
+++ include/freertos_cpp_util/Mutex_static_recursive.hpp
@@ -0,0 +1,25 @@
+/**
+ * @brief Stack allocated recursvoe mutex
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2019 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
+#pragma once
+
+#include "freertos_cpp_util/Mutex_base.hpp"
+
+class Mutex_static_recursive : public Mutex_base
+{
+public:
+	Mutex_static_recursive()
+	{
+		m_mutex = xSemaphoreCreateRecursiveMutexStatic(&m_mutex_buf);
+	}
+	~Mutex_static_recursive() override
+	{
+
+	}
+protected:
+	StaticSemaphore_t m_mutex_buf;
+};
