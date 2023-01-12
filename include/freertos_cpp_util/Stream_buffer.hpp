36489b4b9e1c9c5a7160f17c5bbc310709381411
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2023-01-12 03:21:03 -0800

    header

--- include/freertos_cpp_util/Stream_buffer.hpp
+++ include/freertos_cpp_util/Stream_buffer.hpp
@@ -1,3 +1,9 @@
+/**
+ * @author Jacob Schloss <jacob@schloss.io>
+ * @copyright Copyright (c) 2023 Jacob Schloss. All rights reserved.
+ * @license Licensed under the 3-Clause BSD license. See LICENSE for details
+*/
+
 #pragma once
 
 #include "FreeRTOS.h"
